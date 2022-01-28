2dc612ea4ff1fe067dfa44fead73020f5450af2c
sianl2022 <sian_long@hotmail.co.uk>
2022-01-28 16:52:59 +0000

    still to do remaning part3

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -11,3 +11,10 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items = items)
+
+@app.route('/create-todo', methods=['POST'])
+def create_new_todo():
+    new_todo_title = request.form['todo-name']
+    add_item(new_todo_title)
+    return index()
+    
