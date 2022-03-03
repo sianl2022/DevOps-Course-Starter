4f25ca82a1d330f676dea52f00952e765d305d0a
sianl2022 <sian_long@hotmail.co.uk>
2022-03-03 16:45:05 +0000

    Working TODO app

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
+import requests
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,7 +11,27 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+
+    url = "https://api.trello.com/1/boards/AbpSrXDQ/lists"
+
+    os.getenv("TRELLO_API_KEY")
+
+    querystring = {
+    "key":os.getenv("TRELLO_API_KEY"),
+    "token":os.getenv("TRELLO_API_TOKEN"),
+    "cards": "open" }
+
+    response = requests.request("GET", url, params=querystring)
+
+    response_json = response.json()
+
+    items = []
+
+    for trello_list in response_json:
+        for card in trello_list['cards']:
+            card['status'] = trello_list['name']
+            items.append(card)
+
     return render_template('index.html', items = items)
 
 @app.route('/create-a-todo', methods=['POST'])
@@ -17,4 +39,6 @@ def create_new_todo():
     new_todo_title = request.form['todo-name']
     add_item(new_todo_title)
     return index()
+    
+
     
--- trello_api_call.py
+++ trello_api_call.py
@@ -2,13 +2,37 @@ import requests
 import dontenv
 import os
 
-url = "https://api.trello.com/1/members/me/boards"
+
+def mark_card_done(card_id):
+    params = {
+        'idList': idList,
+        'key' : os.getenv('TRELLO_KEY'),
+        'token' : os.getenv('TRELLO_TOKEN') 
+    }
+    requests.post('https://api.trello.com/1/cards/', params=params)
+
+def create_card(idList, name):
+    params = {
+        'idList' : idList,
+        'name' : name,
+        'key' : os.getenv('TRELLO_KEY'),
+        'token' : os.getenv('TRELLO_TOKEN')
+    }
+    request.post('https://api.trello.com/1/cards/{card_id}', params=params)
+
+
+
+url = "https://api.trello.com/1/boards/AbpSrXDQ/lists"
 
 env_file = dotenv.load_dotenv(".env")
 
 os.getenv("TRELLO_API_KEY")
 
-querystring = {"key":os.getenv("TRELLO_API_KEY"),"token":os.getenv("TRELLO_API_TOKEN")}
+querystring = {
+"key":os.getenv("TRELLO_API_KEY"),
+"token":os.getenv("TRELLO_API_TOKEN"),
+"cards":"open"
+}
 
 response = requests.request("GET", url, params=querystring)
 
