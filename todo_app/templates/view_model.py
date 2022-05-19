02bd1557eaa9d765ac9f1e631f0c16ca0c40e6bb
sianl2022 <sian_long@hotmail.co.uk>
2022-05-19 16:23:02 +0000

    Added more test doing and done

--- todo_app/templates/view_model.py
+++ todo_app/templates/view_model.py
@@ -16,10 +16,19 @@ class ViewModel:
         
     @property
     def doing_items(self):
+        doing_items=[]
+        for item in self._items:
+            if item.status == 'doing':
+                doing_items.append(item)
+        return doing_items
         # using self._items, pick out and return only the items that are in the 'doing' status 
-        pass
+        
 
     @property
     def done_items(self):
+        done_items=[]
+        for item in self._items:
+            if item.status == 'done':
+                done_items.append(item)
+        return done_items
         # using self._items, pick out and return only the items that are in the 'done' status 
-        pass
--- todo_app/test_example.py
+++ todo_app/test_example.py
@@ -1,10 +1,39 @@
 from todo_app.templates.view_model import ViewModel
 from todo_app.data.item import Item
 
-def test_view_can_only_ret_todo_items():
-    items= [Item('1234', 'work', 'todo')]
+def test_view_can_ret_todo_items():
+    items= [Item('1234', 'start', 'todo')]
     item_view_model = ViewModel(items)
 
     todo_items = item_view_model.todo_items
 
     assert len (todo_items) == 1
+
+def test_view_can_ret_doing_items():
+    items= [Item('5678', 'middle', 'doing')]
+    item_view_model = ViewModel(items)
+
+    doing_items = item_view_model.doing_items
+
+    assert len (doing_items) == 1
+
+def test_view_can_ret_done_items():
+    items= [Item('91011', 'end', 'done')]
+    item_view_model = ViewModel(items)
+
+    done_items = item_view_model.done_items
+
+    assert len (done_items) == 1
+
+def test_view_can_ret_all_done_items():
+    items= [Item('91011', 'end', 'done')]
+    items= [Item('12489', 'end1', 'done')]
+
+    item_view_model = ViewModel(items)
+
+    done_items = item_view_model.done_items
+
+    assert len (done_items) == 2
+
+    
+
